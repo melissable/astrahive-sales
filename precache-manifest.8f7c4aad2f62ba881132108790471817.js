f6a8de5355fffcdd53b36fb5b4bf42021fac36a1
Melissa <melissa@sightsource.net>
2019-07-22 15:13:32 -0400

    Updates

---
+++ precache-manifest.8f7c4aad2f62ba881132108790471817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8012e29d3190652ffd4bcd50ed6ad234",
+    "url": "/astrahive-sales/index.html"
+  },
+  {
+    "revision": "a17af421bcbde017e009",
+    "url": "/astrahive-sales/static/css/2.b449cd6b.chunk.css"
+  },
+  {
+    "revision": "fe341f5d9d3efdc52def",
+    "url": "/astrahive-sales/static/css/main.23507b24.chunk.css"
+  },
+  {
+    "revision": "a17af421bcbde017e009",
+    "url": "/astrahive-sales/static/js/2.d3432ee1.chunk.js"
+  },
+  {
+    "revision": "fe341f5d9d3efdc52def",
+    "url": "/astrahive-sales/static/js/main.509f1ea4.chunk.js"
+  },
+  {
+    "revision": "8cfe4cdcaa1f68146c47",
+    "url": "/astrahive-sales/static/js/runtime~main.f1c15820.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/melissable/astrahive-sales/precache-manifest.7a8fc04d880f62049215df79524cf963.js"
+  "/astrahive-sales/precache-manifest.8f7c4aad2f62ba881132108790471817.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/melissable/astrahive-sales/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/astrahive-sales/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
