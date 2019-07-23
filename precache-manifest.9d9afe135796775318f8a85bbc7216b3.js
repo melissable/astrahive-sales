fc3549cc18c8278395f3635f039a42deb2def495
Melissa <melissa@sightsource.net>
2019-07-22 22:55:58 -0400

    Updates

---
+++ precache-manifest.9d9afe135796775318f8a85bbc7216b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00dba9eac219ab333efc4fad2b02b22f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a17af421bcbde017e009",
+    "url": "/static/css/2.b449cd6b.chunk.css"
+  },
+  {
+    "revision": "43dbb0c26afd7be19e9e",
+    "url": "/static/css/main.23507b24.chunk.css"
+  },
+  {
+    "revision": "a17af421bcbde017e009",
+    "url": "/static/js/2.d3432ee1.chunk.js"
+  },
+  {
+    "revision": "43dbb0c26afd7be19e9e",
+    "url": "/static/js/main.8b2da363.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
