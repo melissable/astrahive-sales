98019207f7f0d9933ee9a1d646cf83fc888b022c
Melissa <melissa@sightsource.net>
2019-09-14 13:01:01 -0400

    working data structure

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 export { default as chartjs } from './chartjs';
+export { default as data } from './data';
 export { default as helpers } from './helpers';
 export { default as settings } from './settings';
 export { default as validators } from './validators';
