5feef4d8f6a6fcca472d99fd1619df25cc320c7d
Melissa <melissa@sightsource.net>
2019-09-14 14:35:43 -0400

    added svg icon functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,5 @@ export { default as chartjs } from './chartjs';
 export { default as data } from './data';
 export { default as helpers } from './helpers';
 export { default as settings } from './settings';
+export { default as svgs } from './svgs';
 export { default as validators } from './validators';
